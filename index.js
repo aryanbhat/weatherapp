3f803a388a5c7bdf10f5a812d31075107647cb83
aryanbhat <aryanbhat324@gmail.com>
2023-06-27 15:56:29 +0530

    intial

---
+++ index.js
@@ -0,0 +1,25 @@
+const key = "47b542aba39d9abd0cd5b7d37d36077d";
+function getData(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`).then(callback);
+}
+
+function callback(res){
+    res.json().then((data)=>{
+        console.log(data);
+    })
+}
+
+const limit = 5;
+function getLatLon(){
+    let city = "Delhi";
+    let state = "";
+    let country = "";
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=${limit}&appid=${key}`).then(callbackLatLon);
+}
+function callbackLatLon(res){
+    res.json().then((data)=>{
+        console.log(data);
+        getData(data[0].lat,data[0].lon);
+    })
+}
+getLatLon();
